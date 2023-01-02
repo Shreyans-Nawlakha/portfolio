301864b6a65a0ccf8a2e8562dec8c58b35908fa9
Shreyans-Nawlakha <86418929+Shreyans-Nawlakha@users.noreply.github.com>
2023-01-02 12:10:01 +0530

    changes commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+      <a href="https://www.instagram.com/_shreyans____/" className="home__social-icon" target="_blank" rel="noreferrer">
+        <i class="uil uil-instagram"></i></a>
+      <a href="https://github.com/Shreyans-Nawlakha" className="home__social-icon" target="_blank" rel="noreferrer">
+      <i class="uil uil-github-alt"></i>
+      </a>
+      <a href="#linkedin" className="home__social-icon" target={"_blank"}>
+      <i class="uil uil-linkedin"></i>
+      </a>
+    </div>
+  )
+}
+
+export default Social
